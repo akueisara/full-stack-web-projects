42c9fc7800cd9aa81fa67f0358aab2f86053e69c
Kuei-Jung Hu <akueisara39@gmail.com>
2020-07-24 19:23:12 +0900

    Build a CRUD app with SQLAlchemy - part 1

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kuei@localhost:5432/todoapp'
@@ -17,13 +18,24 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description', '')
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    # redirect to 'index' with data
-    # return redirect(url_for('index', data))
-    return redirect(url_for('index'))
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description2=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort (400)
+    else:
+        return jsonify(body)
 
 # @app.route('/<data>')
 # def index(data):
