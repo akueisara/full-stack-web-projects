bee23a140d12e0b93171d272403a4d86884ef8df
Kuei-Jung Hu <akueisara39@gmail.com>
2020-07-25 22:29:47 +0900

    Update and delete a Todo item

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, jsonify, abort
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kuei@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
@@ -24,9 +26,11 @@ def create_todo():
     body = {}
     try:
         description = request.get_json()['description']
-        todo = Todo(description2=description)
+        todo = Todo(description=description)
         db.session.add(todo)
         db.session.commit()
+        body['id'] = todo.id
+        body['completed'] = todo.completed
         body['description'] = todo.description
     except:
         error = True
@@ -39,8 +43,34 @@ def create_todo():
     else:
         return jsonify(body)
 
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({ 'success': True})
+
 # @app.route('/<data>')
 # def index(data):
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())
+    return render_template('index.html', data=Todo.query.order_by('id').all())
