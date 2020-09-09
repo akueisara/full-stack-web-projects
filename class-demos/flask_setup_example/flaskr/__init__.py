52289e76adac878cc7fd05caa1778b270c4267de
Kuei-Jung Hu <akueisara39@gmail.com>
2020-09-09 22:48:28 +0900

    Flask-CORS sample

--- class-demos/flask_setup_example/flaskr/__init__.py
+++ class-demos/flask_setup_example/flaskr/__init__.py
@@ -1,33 +1,74 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 import os
+from models import setup_db, Plant
+from flask_cors import CORS
 
 def create_app(test_config=None):
-    # create and configure
+    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
-    )
+    setup_db(app)
+    CORS(app)
+    # Resource-Specific Usage
+    # CORS(app, resources={r"*/api/*": {origins: '*'}})
+    
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    # )
 
-    if test_config is None:
-        # load the instance config, it if exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    # if test_config is None:
+    #     # load the instance config, it if exists, when not testing
+    #     app.config.from_pyfile('config.py', silent=True)
+    # else:
+    #     # load the test config if passed in
+    #     app.config.from_mapping(test_config)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # try:
+    #     os.makedirs(app.instance_path)
+    # except OSError:
+    #     pass
 
-# a simple page that says hello
-    @app.route('/')
-    def hello():
-        return jsonify({'message': 'HELLO WORLD'})
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
+
+    # a simple page that says hello
+    # @app.route('/')
+    # Route-Specific Usage
+    # @cross_origin
+    # def hello():
+    #     return jsonify({'message': 'HELLO WORLD'})
 
     @app.route('/smiley')
     def smiley():
         return ':)'
+
+    @app.route('/plants', methods=['GET'])
+    def get_plants():
+        page = request.args.get('page', 1, type=int)
+        start = (page -1) * 10
+        end = start + 10
+        plants = Plant.query.all()
+        formatted_plants = [plant.format() for plant in plants]
+        return jsonify({
+            'success':True,
+            'plants': formatted_plants[start:end],
+            'total_plants': len(formatted_plants)
+        })
+
+    @app.route('/plants/<int:plant_id>')
+    def get_specific_plant(plant_id):
+        plant = Plant.query.filter(Plant.id == plant_id).one_or_none()
+
+        if plant is None:
+            abort(404)
+        else:    
+            return jsonify({
+                'success':True,
+                'plants': plant.format()
+            })
 
     return app
