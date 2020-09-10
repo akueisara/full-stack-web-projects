b87d7d471470dbfcfa7bf14cf1bdcd7a27791346
Kuei-Jung Hu <akueisara39@gmail.com>
2020-09-11 02:15:30 +0900

    Update the flask set up example for endpoint /plants/<int:plant_id>

--- class-demos/flask_setup_example/flaskr/__init__.py
+++ class-demos/flask_setup_example/flaskr/__init__.py
@@ -71,4 +71,94 @@ def create_app(test_config=None):
                 'plants': plant.format()
             })
 
+    @app.route('/plants/<int:plant_id>', methods=['POST'])
+    def create_plant():
+        body = request.get_json()
+
+        try:
+            plant_name = body.get('name')
+            plant_scientific = body.get('scientific_name')
+            plant_color = body.get('primary_color')
+            plant_poisonous = body.get('is_poisonous')
+
+            new_plant = Plant(
+                name = plant_name,
+                scientific_name = plant_scientific,
+                primay_color = plant_color,
+                is_poisonous = plant_poisonous
+            )
+
+            new_plant.insert()
+
+            return jsonify({
+                'success': True,
+                'id': new_plant.id
+            })
+
+        except:
+            abort(422)
+
+
+    @app.route('/plants/<int:plant_id>', methods=['DELETE'])
+    def delete_plant(plant_id):
+        plant = Plant.query.filter(Plant.id == plant_id).one_or_none()
+
+        if plant is None:
+            abort(404)
+        else:
+            plant.delete()
+
+            return jsonify({
+                'success': True,
+                'id': plant_id,
+            })
+
+    @app.route('/plants/<int:plant_id>', methods=['PATCH'])
+    def modify_plant(plant_id):
+        body = request.get_json()
+        
+        plant = Plant.query.filter(Plant.id == plant_id).one_or_none()
+
+        if plant is None:
+            abort(404)
+
+        else:
+            try:
+                new_name = body.get('name', None)
+                new_scientific = body.get('scientific_name', None)
+                new_color = body.get('primary_color', None)
+                new_poisonous = body.get('is_poisonous', None)
+
+                plant.name = new_name or plant.name
+                plant.scientific_name = new_scientific or plant.scientific_name
+                plant.primary_color = new_color or plant.primary_color
+                if new_poisonous:
+                    plant.is_poisonous = bool(new_poisonous)
+
+                plant.update()
+
+                return jsonify({
+                    'success': True,
+                    'id': plant.id
+                })
+            
+            except:
+                abort(422)
+            
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
     return app
