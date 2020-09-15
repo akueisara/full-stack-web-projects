3ffda0205a2eb81266c9fadb3d0c9deedc7b5ff0
Kuei-Jung Hu <akueisara39@gmail.com>
2020-09-15 21:07:05 +0900

    Practice: applying skills in Flask

--- class-demos/flask-walkthroughs/app.py
+++ class-demos/flask-walkthroughs/app.py
@@ -1,5 +1,72 @@
 from flask import Flask, request, abort
 from functools import wraps
+from jose import jwt
+from urllib.request import urlopen
+import json
+
+AUTH0_DOMAIN = 'akueisara.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'image'
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+def verify_decode_jwt(token):
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
+
 
 def get_token_auth_header():
     if 'Authorization' not in request.headers:
@@ -20,7 +87,11 @@ def requires_auth(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
         jwt = get_token_auth_header()
-        return f(jwt, *args, **kwargs)
+        try:
+            payload = verify_decode_jwt(jwt)
+        except:
+            abort(401)
+        return f(payload, *args, **kwargs)
     return wrapper
 
 
